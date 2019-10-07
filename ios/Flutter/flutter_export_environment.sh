54b13ddbf6305a90630e0381f2b1d682a2da56a4
Subbu <Pampana>
2019-10-07 10:47:01 +0530

    plugin updated

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Applications/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/itinfra/Documents/office/howzat/playfantasy_native"
+export "FLUTTER_TARGET=/Users/itinfra/Documents/office/howzat/playfantasy_native/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Applications/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=3.54.0"
+export "FLUTTER_BUILD_NUMBER=4"
