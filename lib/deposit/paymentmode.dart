5e450603fe54f80edca4860badc68c709f426421
Subbu <Pampana>
2019-10-16 10:34:13 +0530

    version updated

--- lib/deposit/paymentmode.dart
+++ lib/deposit/paymentmode.dart
@@ -194,10 +194,6 @@ class ChoosePaymentModeState extends State<ChoosePaymentMode> {
       }
     } catch (e) {
       showLoader(false);
-      ActionUtil().showMsgOnTop(
-                "Payment cancelled please retry transaction. In case your money has been deducted, please contact customer support team!",
-                context);
-
     }
     return "";
   }
